cbbb60989472a12d5d42bc31967428d5af6d11a9
pradeep <pradeep.kumar44@gmail.com>
2022-06-27 11:54:03 +0530

    committing the web applicaion

--- src/main/java/com/classpath/orderrestapi/config/AppConfiguration.java
+++ src/main/java/com/classpath/orderrestapi/config/AppConfiguration.java
@@ -1,15 +1,39 @@
 package com.classpath.orderrestapi.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
-	
-	@Bean
-	public User user() {
-		return new User();
-	}
+
+    @ConditionalOnProperty(prefix = "app", value = "loadUser", havingValue = "true", matchIfMissing = true)
+    @Bean
+    User user() {
+        return new User();
+    }
+    
+    @ConditionalOnBean(name="user")
+    @Bean
+    User userBasedOnBean() {
+        return new User();
+    }
+    
+    @ConditionalOnMissingBean(name="user")
+    @Bean
+    User userBasedOnMissingBean() {
+        return new User();
+    }
+    
+    @ConditionalOnMissingClass(value = "com.xyz.ABC")
+    @Bean
+    User userBasedOnClass() {
+        return new User();
+    }
 
 }
 
