859b4469288488b8b5750fb8d28653459754b340
pradeep <pradeep.kumar44@gmail.com>
2022-06-28 12:54:44 +0530

    committing the swagger implementation

--- src/main/java/com/classpath/orderrestapi/OrderRestApiApplication.java
+++ src/main/java/com/classpath/orderrestapi/OrderRestApiApplication.java
@@ -3,7 +3,14 @@ package com.classpath.orderrestapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Orders API - V1", contact = @Contact(email = "support@bla.com"))
+		)
 public class OrderRestApiApplication {
 
 	public static void main(String[] args) {
